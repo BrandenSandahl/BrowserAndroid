ceeadc4d1cf2f8e955a02dd841f39b3ea89387e7
BrandenSandahl <branden157@gmail.com>
2016-02-17 12:10:25 -0500

    Fixed browser launching in a new app

--- app/src/main/java/com/theironyard/browserandroid/MainActivity.java
+++ app/src/main/java/com/theironyard/browserandroid/MainActivity.java
@@ -1,12 +1,15 @@
 package com.theironyard.browserandroid;
 
+import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Button;
 import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     EditText addressBar;
     Button backButton, forwardButton, goButton;
@@ -25,7 +28,32 @@ public class MainActivity extends AppCompatActivity {
         goButton = (Button) findViewById(R.id.goButton);
         webview = (WebView) findViewById(R.id.webView);
 
+        backButton.setOnClickListener(this);
+        forwardButton.setOnClickListener(this);
+        goButton.setOnClickListener(this);
+
+        webview.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                addressBar.setText(url);
+            }
+        });
+    }
 
 
+
+    @Override
+    public void onClick(View v) {
+        if (v == backButton) {
+            webview.goBack();
+        } else if (v == forwardButton) {
+            webview.goForward();
+        } else if (v == goButton) {
+            String url = addressBar.getText().toString();
+            if (!url.startsWith("http")) {
+                url = "http://" + url;
+            }
+            webview.loadUrl(url);
+        }
     }
 }
